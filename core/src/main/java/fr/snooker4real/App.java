d5c38940771037bc5ac30640a29a90c29193e40e
Cindano Jonathan <65620947+Snooker4Real@users.noreply.github.com>
2021-07-19 15:02:51 +0200

    Initial commit

---
+++ core/src/main/java/fr/snooker4real/App.java
@@ -0,0 +1,8 @@
+package fr.snooker4real;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
---
+++ service/src/test/java/fr/snooker4real/AppTest.java
@@ -0,0 +1,12 @@
+package fr.snooker4real;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
